273b48cd6265441183f258ebca81649e6f843e9c
Jason Best <jason.best@swiftpage.com>
2014-04-24 10:40:07 -0700

    Started typescript support.

---
+++ src-typescript/Application.js
@@ -0,0 +1,8 @@
+﻿/// <reference path="dojo.d.ts"/>
+define(["require", "exports", 'dojo/_base/declare'], function(require, exports, dojoDeclare) {
+    var app = dojoDeclare("Mobile.SalesLogix.Application", [Application], {});
+
+    
+    return app;
+});
+//# sourceMappingURL=Application.js.map
---
+++ src-typescript/Application.ts
@@ -0,0 +1,20 @@
+﻿/// <reference path="dojo.d.ts"/>
+
+import json = require('dojo/json');
+import array = require('dojo/_base/array');
+import connect = require('dojo/_base/connect');
+import aspect = require('dojo/aspect');
+import dojoDeclare = require('dojo/_base/declare');
+import lang = require('dojo/_base/lang');
+import hash = require('dojo/hash');
+import has = require('dojo/has');
+import domConstruct = require('dojo/dom-construct');
+import sniff = require('dojo/sniff');
+import mobileSniff = require('dojox/mobile/sniff');
+import ReUI = require('Sage/Platform/Mobile/ReUI/main');
+
+var app = dojoDeclare("Mobile.SalesLogix.Application", [Application], {
+});
+
+
+export = app;
